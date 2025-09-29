89c5823df6fbfc93000fcace41abf472d5779979
willanisaac <137942741+willanisaac@users.noreply.github.com>
2025-09-29 03:52:15 +0000

    prueba de concpeto con github codespaces

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kapi_database
+coleccion de los diseños y esquemas de base de datos a usar en nuestra solucion, cada carpets estara acompañada de los scripts de sql y un pdf con el diseño entidad relacion.
+
+toda la solucion esta en postgresql
---
+++ nomina/esquema_nomina.sql
@@ -0,0 +1,23 @@
+-- C
+CREATE SCHEMA IF NOT EXISTS calc;
+
+SET search_path TO calc;
+-- Tabla Empleador
+CREATE TABLE Empleador (
+	idEmpleador INT NOT NULL,
+	codEmpleador VARCHAR(20) NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	telefono VARCHAR(20),
+    email VARCHAR(100),
+	direccion VARCHAR(300),
+	descripcion TEXT,
+	contacto VARCHAR(100),
+	telefono_contacto VARCHAR(20),
+	email_contacto VARCHAR(100),
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	usuario_creacion VARCHAR(50),
+	fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	usuario_actualizacion VARCHAR(50),
+	PRIMARY KEY (idEmpleador, codEmpleador)
+);
+
