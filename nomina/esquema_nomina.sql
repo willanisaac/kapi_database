000de58c062b4e7a45cc8bd0ff2d2e815f7c29a0
willanisaac <willan.tuquerrez@outlook.com>
2025-11-04 03:13:32 -0500

    esquema(parcial-nomina): trabajo de nomina con yember

--- nomina/esquema_nomina.sql
+++ nomina/esquema_nomina.sql
@@ -1,7 +1,8 @@
--- C
+-- Creación del esquema de nómina
 CREATE SCHEMA IF NOT EXISTS calc;
 
 SET search_path TO calc;
+
 -- Tabla Empleador
 CREATE TABLE Empleador (
 	idEmpleador INT NOT NULL,
@@ -23,7 +24,7 @@ CREATE TABLE Empleador (
 
 -- Tabla Empleado
 CREATE TABLE Empleado (
-	idEmpleado INT NOT NULL,
+	idEmpleado BIGINT NOT NULL ,
 	identificacion VARCHAR(30) NOT NULL,
 	idEmpleador INT NOT NULL,
 	codEmpleador VARCHAR(20) NOT NULL,
@@ -43,3 +44,86 @@ CREATE TABLE Empleado (
 	FOREIGN KEY (idEmpleador, codEmpleador) REFERENCES Empleador(idEmpleador, codEmpleador)
 );
 
+-- Tabla Contrato
+CREATE TABLE Contrato (
+	idContrato BIGINT NOT NULL ,
+	idEmpleado BIGINT NOT NULL,
+	identificacion VARCHAR(30) NOT NULL,
+	idEmpleador INT NOT NULL,
+	codEmpleador VARCHAR(20) NOT NULL,
+	tipo_contrato VARCHAR(50) NOT NULL,
+	detalle TEXT,
+	descripcion VARCHAR(300),
+	fecha_inicio DATE NOT NULL,
+	fecha_fin DATE,
+	estado VARCHAR(20) NOT NULL,
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	usuario_creacion VARCHAR(50),
+	fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	usuario_actualizacion VARCHAR(50),
+	PRIMARY KEY (idContrato),
+	FOREIGN KEY (idEmpleado, identificacion) REFERENCES Empleado(idEmpleado, identificacion),
+	FOREIGN KEY (idEmpleador, codEmpleador) REFERENCES Empleador(idEmpleador, codEmpleador)
+)
+
+-- Tabla Ademdum
+CREATE TABLE Ademdum (
+	idContrato BIGINT NOT NULL,
+	fecha_ademdum DATE NOT NULL,
+	tipo_ademdum VARCHAR(100) NOT NULL,
+	detalle TEXT,
+	descripcion VARCHAR(300),
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	usuario_creacion VARCHAR(50),
+	fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	usuario_actualizacion VARCHAR(50),
+	FOREIGN KEY (idContrato) REFERENCES Contrato(idContrato)
+);
+
+-- Tabla Nomina
+CREATE TABLE Nomina (
+	idNomina BIGINT NOT NULL ,
+	idContrato INT NOT NULL,
+	periodo_inicio DATE NOT NULL,
+	periodo_fin DATE NOT NULL,
+	salario_base DECIMAL(10,2) NOT NULL,
+	bonificaciones DECIMAL(10,2) DEFAULT 0,
+	deducciones DECIMAL(10,2) DEFAULT 0,
+	total_pagado DECIMAL(10,2) NOT NULL,
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	usuario_creacion VARCHAR(50),
+	fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	usuario_actualizacion VARCHAR(50),
+	PRIMARY KEY (idNomina),
+	FOREIGN KEY (idContrato) REFERENCES Contrato(idContrato)
+);
+
+-- Tabla Descuentos
+CREATE TABLE Descuentos (
+	idNomina BIGINT NOT NULL ,
+	tipo_descuento VARCHAR(100) NOT NULL,
+	monto DECIMAL(10,2) NOT NULL,
+	descripcion TEXT,
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	usuario_creacion VARCHAR(50),
+	fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	usuario_actualizacion VARCHAR(50),
+	FOREIGN KEY (idNomina) REFERENCES Nomina(idNomina)
+);
+
+-- Tabla Ingresos
+CREATE TABLE Ingresos (
+	idNomina BIGINT NOT NULL ,
+	tipo_bonificacion VARCHAR(100) NOT NULL,
+	monto DECIMAL(10,2) NOT NULL,
+	descripcion TEXT,
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	usuario_creacion VARCHAR(50),
+	fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	usuario_actualizacion VARCHAR(50),
+	FOREIGN KEY (idNomina) REFERENCES Nomina(idNomina)
+);
+
+
+
+	
